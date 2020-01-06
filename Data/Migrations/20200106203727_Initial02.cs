11009e05eb976507ebda19893d8d57517c068e99
beikoo <57067511+beikoo@users.noreply.github.com>
2020-01-06 22:39:06 +0200

    Seed and Done with ProductManager

---
+++ Data/Migrations/20200106203727_Initial02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class Initial02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2020, 1, 6, 22, 37, 27, 255, DateTimeKind.Local).AddTicks(1672));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2019, 12, 26, 17, 24, 31, 711, DateTimeKind.Local).AddTicks(3657));
+        }
+    }
+}

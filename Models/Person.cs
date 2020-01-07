2df0ecafb1183cb3840b0f01a87ce62b728dd1cb
beikoo <57067511+beikoo@users.noreply.github.com>
2020-01-07 21:37:01 +0200

    hh

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Models
 {
+    [Table("Users")]
     public class Person:BaseModel
     {
         public string FirstName { get; set; }
--- WebStoreApi/Contollers/ProductController.cs
+++ WebStoreApi/Contollers/ProductController.cs
@@ -9,6 +9,7 @@
 
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private ProductManager manager;

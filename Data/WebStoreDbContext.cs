7e6e4570b44052f95c481c2c05e9f94c8c485ffa
airfanBG <damyanovdaniel@yahoo.com>
2020-01-08 08:53:45 +0200

    People to Users

--- Data/WebStoreDbContext.cs
+++ Data/WebStoreDbContext.cs
@@ -16,7 +16,7 @@ namespace Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
-        public DbSet<Person> People { get; set; }
+        public DbSet<Person> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<SalesOrder> Sales { get; set; }
